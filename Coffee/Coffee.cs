04fa5850b5e42b0af359f01facd978166a6fcef3
Lukasz Mroz <mrozikpl@gmail.com>
2020-12-08 09:53:43 +0100

    Modeling information as data

---
+++ Coffee/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace Coffee
+{
+  public class Coffee
+  {
+    private string _kind;
+    private int _price; // zł per kilo
+    private int _weight; // kilograms
+
+    public Coffee(string kind, int price, int weight)
+    {
+      _kind = kind;
+      _price = price;
+      _weight = weight;
+    }
+  }
+}
--- Dates/Program.cs
+++ Dates/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-namespace Coffee
+namespace Dates
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Coffee america = new Coffee("americana", 12, 1);
-      Coffee frappen = new Coffee("frappe", 10, 3);
       Console.WriteLine("Hello World!");
     }
   }
